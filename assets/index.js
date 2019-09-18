996681237435f092eba32ecd033281d1be171f2e
rjfm <javier.fuentesm@hotmail.com>
2019-09-17 23:33:11 -0500

    Entendiendo el this

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,10 @@
-import MediaPlayer from './MediaPlayer.js'
-import AutoPlay from './plugins/AutoPlay.js'
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
 
+const video = document.querySelector("video");
+const player = new MediaPlayer({ el: video, /* plugins: [new AutoPlay()] */ });
 
-
-      const video = document.querySelector('video');
-      const player = new MediaPlayer({ el: video ,plugins:[new AutoPlay()] });
-
-      const button = document.getElementById('play');
-      button.onclick = () => player.togglePlay();
-      const button2 = document.getElementById('mute');
-      button2.onclick = () => player.toggleMute();
-
-
+const button = document.getElementById("play");
+button.onclick = () => player.togglePlay();
+const button2 = document.getElementById("mute");
+button2.onclick = () => player.toggleMute();
