0f0336d507b0b76cc295b85beaef0f76f8b08df1
rjfm <jaier.fuentesm@hotmail.com>
2019-09-19 15:46:59 -0500

    Promesas y async a wait ejemplos de formas de peticion en secuencia y en paralelo

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,10 @@
 import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
 
-const video = document.querySelector("video");
+const video = document.querySelector('video');
 const player = new MediaPlayer({ el: video, /* plugins: [new AutoPlay()] */ });
 
-const button = document.getElementById("play");
+const button = document.getElementById('play');
 button.onclick = () => player.togglePlay();
-const button2 = document.getElementById("mute");
+const button2 = document.getElementById('mute');
 button2.onclick = () => player.toggleMute();
