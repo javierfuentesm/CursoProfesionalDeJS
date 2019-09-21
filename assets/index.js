a3804df204aeb1c40a8ea131573c31710dd6486b
javierfuentesm <javier.fuentesm@hotmail.com>
2019-09-20 23:54:23 -0500

    Implementacion de AutoPause cuando deja de ver el video

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,14 @@
 import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
 
-const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const video = document.querySelector("video");
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()]
+});
 
-const button = document.getElementById('play');
+const button = document.getElementById("play");
 button.onclick = () => player.togglePlay();
-const button2 = document.getElementById('mute');
+const button2 = document.getElementById("mute");
 button2.onclick = () => player.toggleMute();
---
+++ assets/plugins/AutoPause.js
@@ -0,0 +1,55 @@
+/* class AutoPause {
+  constructor() {
+    this.threshold = 0.25;
+    //Vamos a establecer el this permanentemente a la instancia del proeycto
+    this.handleIntersection = this.handleIntersection.bind(this);
+  }
+  run(player) {
+    this.player = player;
+    const observer = new IntersectionObserver(this.handleIntersection, {
+      threshold: this.threshold
+    });
+    observer.observe(this.player.media);
+  }
+  handleIntersection(entries) {
+    const entry = entries[0];
+
+    const isVisible = entry.intersectionRatio >= this.threshold;
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+}
+export default AutoPause;
+ */
+
+class AutoPause {
+    constructor() {
+      this.threshold = 0.25;
+      //Vamos a establecer el this permanentemente a la instancia del proyecto o con un arrow function  se puede omitir
+     // this.handleIntersection = this.handleIntersection.bind(this);
+    }
+    run(player) {
+      this.player = player;
+      const observer = new IntersectionObserver(this.handleIntersection, {
+        threshold: this.threshold
+      });
+      observer.observe(this.player.media);
+    }
+
+    //Con el arrow function le pasamos automaticamente el scope de la misma funcon 
+    handleIntersection=entries=> {
+      const entry = entries[0];
+  
+      const isVisible = entry.intersectionRatio >= this.threshold;
+      if (isVisible) {
+        this.player.play();
+      } else {
+        this.player.pause();
+      }
+    }
+  }
+  export default AutoPause;
+  
