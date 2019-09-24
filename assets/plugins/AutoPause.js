a679c66979e24bde42e22497fe41083e79206f93
javierfuentesm <javier.fuentesm@hotmail.com>
2019-09-23 23:33:23 -0500

    Introduccion a tupescript

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -1,52 +1,45 @@
-
-
 class AutoPause {
-    constructor() {
-        this.threshold = 0.25;
-        //Vamos a establecer el this permanentemente a la instancia del proyecto o con un arrow function  se puede omitir
-        // this.handleIntersection = this.handleIntersection.bind(this);
+  constructor() {
+    this.threshold = 0.25;
+    //Vamos a establecer el this permanentemente a la instancia del proyecto o con un arrow function  se puede omitir
+     this.handleIntersection = this.handleIntersection.bind(this);
+     this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
+
+  }
+  run(player) {
+    this.player = player;
+    const observer = new IntersectionObserver(this.handleIntersection, {
+      threshold: this.threshold
+    });
+    observer.observe(this.player.media);
+
+    document.addEventListener("visibilitychange", this.handleVisibilityChange);
+  }
+
+  //Con el arrow function le pasamos automaticamente el scope de la misma funcon
+  handleIntersection(entries) {
+    const entry = entries[0];
+
+    const isVisible = entry.intersectionRatio >= this.threshold;
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
     }
-    run(player) {
-        this.player = player;
-        const observer = new IntersectionObserver(this.handleIntersection, {
-            threshold: this.threshold
-        });
-        observer.observe(this.player.media);
+  }
 
-        document.addEventListener("visibilitychange", this.handleVisibilityChange)
+  handleVisibilityChange (){
+    const isVisible = document.visibilityState === "visible";
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
     }
-
-    //Con el arrow function le pasamos automaticamente el scope de la misma funcon 
-    handleIntersection = entries => {
-        const entry = entries[0];
-
-        const isVisible = entry.intersectionRatio >= this.threshold;
-        if (isVisible) {
-            this.player.play();
-        } else {
-            this.player.pause();
-        }
-    }
-
-    handleVisibilityChange=()=>{
-        const isVisible = document.visibilityState === "visible";
-        if (isVisible) {
-    
-            this.player.play();
-    
-        } else {
-            this.player.pause();
-        }
-}
-
-
+  };
 }
 export default AutoPause;
 
-
-
-
-  /* class AutoPause {
+/* class AutoPause {
   constructor() {
     this.threshold = 0.25;
     //Vamos a establecer el this permanentemente a la instancia del proeycto
@@ -59,7 +52,9 @@ export default AutoPause;
     });
     observer.observe(this.player.media);
   }
-  handleIntersection(entries) {
+
+
+    handleIntersection = entries => {
     const entry = entries[0];
 
     const isVisible = entry.intersectionRatio >= this.threshold;
@@ -68,7 +63,16 @@ export default AutoPause;
     } else {
       this.player.pause();
     }
-  }
+  };
+
+  handleVisibilityChange = () => {
+    const isVisible = document.visibilityState === "visible";
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  };
 }
 export default AutoPause;
  */
---
+++ ejercicios/typescript/index.ts
@@ -0,0 +1,6 @@
+console.log("Hello Typescript");
+function add(a:number,b:number) {
+    return a + b;
+    
+}
+console.log((add(2,3)));
